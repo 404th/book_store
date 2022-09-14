9274410e99bdd57468ea528401c88fa2f76111bc
404th <umarov.doniyor.2002@gmail.com>
2022-09-14 14:01:54 +0500

    Some changes in migrations

--- migrations/postgres/01_create_company_table.up.sql
+++ migrations/postgres/01_create_company_table.up.sql
@@ -1,7 +1,6 @@
 CREATE TABLE IF NOT EXISTS books (
     "id" uuid PRIMARY KEY,
     "name" VARCHAR(255) UNIQUE NOT NULL,
-    "author_id" uuid NOT NULL,
     "about" TEXT NOT NULL,
     "isbn" VARCHAR(255) NOT NULL
 );
